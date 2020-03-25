15687c5a151da2f8915bbb094f971e848c8287d2
unknown <jeff_cox76@hotmail.com>
2020-03-25 11:00:58 -0600

    save01

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
+import React from 'react'
+import './App.css'
 
+function App(){
+    return(
+        <div className='App'>
+            <h1 className='movie-list-title'>The Most Amazing Movie List</h1>
+        </div>
+    )
+}
+export default App
